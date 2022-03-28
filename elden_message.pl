a70a1be5441406fb8353281561e4f4ffcfce7270
mosai <mosai@basilicom>
2022-03-28 16:14:13 -0700

    Added documentation

--- elden_message.pl
+++ elden_message.pl
@@ -43,18 +43,19 @@ my @conjunction = (
 );
 
 my $words_ifile = "elden.txt";
-#my $templ_ifile = "templ.txt";
-#my $cnjct_ifile = "cnjct.txt";
 my @words = read_file($words_ifile);
-#my @templ = read_file($templ_ifile);
-#my @cnjct = read_file($cnjct_ifile);
 
+# Coin flip to determine if we use 1 or 2 templates for the message.
 my $two_templ = int(rand(2));
 
+# Work var setup
 my @template_sel;
 my @word_sel;
 my $word_count = 0;
 
+# Randomly select the templates we will use. Accounts for 1 or 2 templates.
+# Also prevents templates ending with ? or ! from making it in as the first
+# template if we're using 2 templates.
 if($two_templ == 1) {
 	my $temp_sel = "";
 	do {
@@ -62,31 +63,39 @@ if($two_templ == 1) {
 	} while ($temp_sel =~ /[!?]/g);
 	push(@template_sel, $temp_sel);
 }
-
+# Always executes. We need at least 1 template.
 push(@template_sel, $templates[int(rand(@templates))]);
 
+# Get the amount of substitutions we will need to do across all templates.
 foreach (@template_sel) {
 	$word_count += () = $_ =~ /\*\*\*/g;
 }
-
+# Grab the words we will use in our substitutions
 for (1..$word_count) {
 	push(@word_sel, $words[int(rand(@words))]);
 }
 
+
 foreach (@template_sel){
+	# Check how many substitutions we need to do for this line.
 	my $sub_count = () = $_ =~ /\*\*\*/g;
 	my $word = "";
+
+	# If we need to do more than 1 substituion on the template
 	if($sub_count > 1){
 		$word = shift(@word_sel);
 		chomp $word;
 		$_ =~ s/\*\*\*/$word/;
 	}
+
 	$word = shift(@word_sel);
 	chomp $word;
 	$_ =~ s/\*\*\*/$word/;
 }
 
+# Print our output. 
 print $template_sel[0] . "\n";
 if($two_templ == 1){
-	print $conjunction[int(rand(@conjunction))] . ", " . $template_sel[1] . "\n";
+	print $conjunction[int(rand(@conjunction))] . 
+		  ", " . $template_sel[1] . "\n";
 }
